8f859e8601e55de81a45962379cb7d228e0d4462
Johann Chang <mr.changyuheng@gmail.com>
2019-11-26 11:58:41 +0800

    Refine UTF-8 output config

--- src/winicode/__init__.py
+++ src/winicode/__init__.py
@@ -7,4 +7,7 @@ import sys
 
 if os.name == 'nt':
     os.system('chcp 65001 >nul 2>&1')
-    sys.stdout.reconfigure(encoding='utf8')
+    if sys.stdout.encoding != 'utf8':
+        sys.stdout.reconfigure(encoding='utf8')
+    if sys.stderr.encoding != 'utf8':
+        sys.stderr.reconfigure(encoding='utf8')
